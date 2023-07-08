4c786902f6728d32296a81090a0077fd86548354
New_2000 <89688886+NewTanachot@users.noreply.github.com>
2023-07-08 23:46:42 +0700

    Update automatic Prod and Dev .env

---
+++ app/api/server/branchName/route.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from 'next/server'
+import { execSync } from 'child_process';
+
+export async function GET() : Promise<NextResponse> {
+
+    try {
+
+        const result = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
+        console.log(result)
+        return NextResponse.json(result, { status: 200 });
+    } 
+    catch (error) {
+
+        return NextResponse.json(null, { status: 200 });
+    }
+}
