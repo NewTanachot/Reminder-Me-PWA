e6c6086f20ca02bff566b19f41d42de9367c4c7c
New_2000 <TanachotNew2543@gmail.com>
2023-07-09 01:32:14 +0700

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import appIcon from '@/public/image/web-icon/favicon.ico';
 import appleAppIcon from '@/public/image/apple-icon/appstore_v2.png';
 import { Metadata } from 'next'
 import { Athiti } from 'next/font/google' // Itim
+import Head from 'next/head';
 
 const googleFont = Athiti({
     weight: '600',
@@ -13,9 +14,8 @@ export const metadata: Metadata = {
     title: 'Reminder-Me',
     description: 'Reminder-Me place remind and suggest by distance.',
     viewport: { 
-        width: "device-width", 
-        // open this cause viewport bug...
-        // initialScale: 1
+        width: "device-width",
+        userScalable: false,
     },
     icons: {
         icon: appIcon.src,
@@ -24,13 +24,18 @@ export const metadata: Metadata = {
     appleWebApp: { 
         capable: true, 
         title: "Reminder Me", 
-        statusBarStyle: "black-translucent" 
+        statusBarStyle: "black-translucent"
     }
 }
 
 export default function RootLayout({ children, }: { children: React.ReactNode }) {
     return (
         <html lang="en">
+            <Head>
+                <link rel="manifest" href="/manifest.json" />
+                <link rel="apple-touch-icon" href="/icon.png"></link>
+                <meta name="theme-color" content="#fff" />
+            </Head>
             <body className={googleFont.className}>{children}</body>
         </html>
     )
