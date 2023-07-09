003e2d0aaa55f718c36c6a8d11a9f2623cdf1642
New_2000 <TanachotNew2543@gmail.com>
2023-07-10 01:16:52 +0700

    Test PWA bug from IOS safari [APPLE NOT PROVIDE] - (10/7/23)

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import appIcon from '@/public/image/web-icon/favicon.ico';
 import appleAppIcon from '@/public/image/apple-icon/appstore_v2.png';
 import { Metadata } from 'next'
 import { Athiti } from 'next/font/google' // Itim
-import Head from 'next/head';
 
 const googleFont = Athiti({
     weight: '600',
@@ -31,11 +30,6 @@ export const metadata: Metadata = {
 export default function RootLayout({ children, }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <Head>
-                <link rel="manifest" href="/manifest.json" />
-                <link rel="apple-touch-icon" href="/icon.png"></link>
-                <meta name="theme-color" content="#fff" />
-            </Head>
             <body className={googleFont.className}>{children}</body>
         </html>
     )
