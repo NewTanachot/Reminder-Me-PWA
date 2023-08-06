044bc54b49c54d53ecacd230fef5b35c4b8b16b5
New_2000 <89688886+NewTanachot@users.noreply.github.com>
2023-08-06 22:02:15 +0700

    test tag Link

--- component/navbar.tsx
+++ component/navbar.tsx
@@ -2,6 +2,7 @@ import { DisplayCurrentPageName } from "@/extension/string_extension";
 import { PwaCurrentPage } from "@/model/enum_model";
 import { INavbarProps } from "@/model/props_model";
 import appIcon from '@/public/image/web-icon/favicon.ico';
+import Link from "next/link";
 
 export default function Navbar({ userName, currentPageName, changeCurrentPage }: INavbarProps) {
 
@@ -12,7 +13,15 @@ export default function Navbar({ userName, currentPageName, changeCurrentPage }:
                     <img className="ms-1 me-2" src={appIcon.src} alt="appIcon" width={40} height={40} />
                     {DisplayCurrentPageName(currentPageName)}
                 </h5>
-                {
+                <Link
+                        // onClick={() => changeCurrentPage(PwaCurrentPage.Login)} 
+                        href="auth/user"
+                        className='m-0 btn btn-success rounded-3 bg-milk-yellow text-peach'
+                    >
+                        <i className="bi bi-person-circle me-2"></i>
+                        {userName}
+                </Link>
+                {/* {
                     currentPageName !== PwaCurrentPage.Login ?
                     <button 
                         onClick={() => changeCurrentPage(PwaCurrentPage.Login)} 
@@ -23,7 +32,7 @@ export default function Navbar({ userName, currentPageName, changeCurrentPage }:
                     </button>
                     :
                     <></>
-                }
+                } */}
             </div>
         </nav>
     )
