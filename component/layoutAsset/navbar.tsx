d35030331dafe5f71770f36f46ae75b18ccae263
New_2000 <TanachotNew2543@gmail.com>
2023-09-21 01:42:21 +0700

    Update all color theme

--- component/layoutAsset/navbar.tsx
+++ component/layoutAsset/navbar.tsx
@@ -3,12 +3,21 @@ import { PwaCurrentPage } from "@/model/enum_model";
 import { INavbarProps } from "@/model/props_model";
 import appIcon from '@/public/image/web-icon/favicon.ico';
 
-export default function Navbar({ currentPageName, orderByDistanceValue, changeOrderByDistanceHandler }: INavbarProps) {
+export default function Navbar({ currentPageName, orderByDistanceValue, changeOrderByDistanceHandler, isDarkTheme }: INavbarProps) {
+
+    let navbarColorTheme = "";
+
+    if (isDarkTheme) {
+        navbarColorTheme = "bg-mainblack";
+    }
+    else {
+        navbarColorTheme = "bg-viridian-green";
+    }
 
     return (
-        <nav className='fixed-top bg-viridian-green text-white px-2 py-2 shadow-bottom rounded-bottom-4'>
+        <nav className={`fixed-top text-cream px-2 py-2 shadow-bottom rounded-bottom-4 ${navbarColorTheme}`}>
             <div className='d-flex justify-content-between align-items-center'>
-                <h5 className='m-0 text-peach ps-0'> 
+                <h5 className='m-0 ps-0'> 
                     <img className="ms-1 me-2" src={appIcon.src} alt="appIcon" width={40} height={40} />
                     {DisplayCurrentPageName(currentPageName)}
                 </h5>
--- component/listPageAsset/userInfo.tsx
+++ component/listPageAsset/userInfo.tsx
@@ -1,18 +1,28 @@
 import { IUserInfoProps } from "@/model/props_model";
 
-export default function UserInfo({ username, location }: IUserInfoProps) {
+export default function UserInfo({ username, location, isDarkTheme }: IUserInfoProps) {
 
     const displayUsername = username.length < 5 ? username : `${username.substring(0,5)}.. `;
 
+    let userInfoColorTheme = "";
+
+    // check theme color
+    if (isDarkTheme) {
+        userInfoColorTheme = "bg-mainblue";
+    }
+    else {
+        userInfoColorTheme = "bg-cornflowerblue";
+    }
+
     return (
-        <div className="card mb-3 shadow-sm rounded-4 bg-cornflowerblue text-white">
+        <div className={`card mb-3 shadow-sm rounded-4 text-white ${userInfoColorTheme}`}>
             <div className="card-body d-flex justify-content-between align-items-center">
                 <p className="m-0">
                     <i className="bi bi-person-circle me-2 h5"></i>
                     {displayUsername} {location.latitude}, {location.longitude}
                 </p>
                 <div>
-                    <button className="btn btn-sm btn-primary bg-gradient">
+                    <button className="btn btn-sm btn-primary bg-gradient text-cream">
                         <i className="bi bi-pin-map-fill"></i>
                     </button>
                 </div>
