64ba2963712881fb9fcab6e9d0b44afb5132a91b
New_2000 <TanachotNew2543@gmail.com>
2023-10-07 01:34:15 +0700

    update UI color theme

--- component/mainpage/loading.tsx
+++ component/mainpage/loading.tsx
@@ -14,9 +14,8 @@ export default function Loading({ isDarkTheme }: ILoadingPageProps) {
 
     return (
         <>
-            <br /> <br /> <br /> <br />
+            <br /> <br /> <br />
             <div className="text-center m-5">
-                {/* <img className="spin-rotate" src={loadingImage.src} alt="loadingImage" width={60} height={60} /> */}
                 <i className={`fa-solid fa-spinner fa-spin-pulse text-loading-size ${loadingColorTheme}`}></i>
             </div>
         </>
