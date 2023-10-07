72afc032622d4c6471a2a5c3fd1fc88bed489e3c
New_2000 <TanachotNew2543@gmail.com>
2023-10-08 01:17:10 +0700

    Fix card status bug and update UI theme setting

--- app/api/place/[id]/route.ts
+++ app/api/place/[id]/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from 'next/server'
 import { PrismaClient } from '@prisma/client'
 import { ResponseModel } from '@/model/responseModel';
 import { GetLastVariableFromPath } from '@/extension/api_extension';
+import { IUpdateCardStatusApiRequest } from '@/model/requestModel';
 
 // Init Prisma connection
 const prisma = new PrismaClient()
@@ -26,17 +27,54 @@ export async function GET(request: Request) : Promise<NextResponse> {
     return NextResponse.json(place, { status: 200 });
 };
 
+export async function PUT(request: Request) : Promise<NextResponse> {
+
+    // get place By Id
+    const placeId = GetLastVariableFromPath(request.url);
+
+    // get body of request
+    const placeStatus: IUpdateCardStatusApiRequest = await request.json();
+
+    try {
+        // delete place from database
+        const updatePlace = await prisma.place.update({
+            where: {
+                id: placeId
+            },
+            data: {
+                isDisable: placeStatus.isDisable
+            }
+        });
+
+        return NextResponse.json(updatePlace, { status: 200 });
+    }
+    catch (error) {
+        return NextResponse.json(<ResponseModel> { 
+            isSuccess: false, 
+            message: "[PUT Place]: Update place status fail. - " + error
+        }, { status: 400 });
+    }
+}
+
 export async function DELETE(request: Request) : Promise<NextResponse> {
 
     // get place By Id
     const placeId = GetLastVariableFromPath(request.url);
 
-    // delete place from database
-    const deleteUser = await prisma.place.delete({
-        where: {
-            id: placeId
-        }
-    });
+    try {
+        // delete place from database
+        const deletePlace = await prisma.place.delete({
+            where: {
+                id: placeId
+            }
+        });
 
-    return NextResponse.json(deleteUser, { status: 200 });
+        return NextResponse.json(deletePlace, { status: 200 });
+    }
+    catch (error) {
+        return NextResponse.json(<ResponseModel> { 
+            isSuccess: false, 
+            message: "[DELETE Place]: Delete place fail. - " + error
+        }, { status: 400 });
+    }
 }
