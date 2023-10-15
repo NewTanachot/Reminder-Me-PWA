44cc1a166a28d3d8238aa14a733eef4a4ef1683a
New_2000 <TanachotNew2543@gmail.com>
2023-10-15 19:17:30 +0700

    add get user with real password api

--- app/api/user/[id]/realpassword/route.ts
+++ app/api/user/[id]/realpassword/route.ts
@@ -1,10 +1,11 @@
-import { NextResponse } from 'next/server'
-import { PrismaClient } from '@prisma/client'
-import { ResponseModel } from '@/model/responseModel';
 import { GetLastVariableFromPath } from '@/extension/api_extension';
+import { DecryptString } from '@/extension/string_extension';
+import { ResponseModel } from '@/model/responseModel';
+import { PrismaClient } from '@prisma/client'
+import { NextResponse } from 'next/server';
 
 // Init Prisma connection
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 // get secret key from .env
 const secretKey = process.env.NEXT_PUBLIC_SECRET_KEY ?? "";
@@ -28,20 +29,7 @@ export async function GET(request: Request) : Promise<NextResponse> {
         return NextResponse.json(<ResponseModel> { isSuccess: false, message: "[Get User]: User not found." }, { status: 400 });
     }
 
+    user.password = DecryptString(user.password, secretKey, user.IV_key);
+
     return NextResponse.json(user, { status: 200 });
 };
-
-export async function DELETE(request: Request) : Promise<NextResponse> {
-
-    // get User By Id
-    const userId = GetLastVariableFromPath(request.url);
-
-    // delete user
-    const deletedUser = await prisma.user.delete({
-        where: {
-            id: userId
-        }
-    })
-
-    return NextResponse.json(deletedUser, { status: 200 });
-}
