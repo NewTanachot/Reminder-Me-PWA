2aa1b9251f7d8cad6380f71698b55ca33d422df7
New_2000 <TanachotNew2543@gmail.com>
2023-10-15 18:50:30 +0700

    Update notfound text color for dark theme

--- component/modalAsset/notfound.tsx
+++ component/modalAsset/notfound.tsx
@@ -1,10 +1,22 @@
-export default function NotFound() {
+import { INotFoundProps } from "@/model/propsModel";
+
+export default function NotFound({ isDarkTheme }: INotFoundProps) {
+
+    let textColorTheme;
+
+    if (isDarkTheme) {
+        textColorTheme = "text-white";
+    }
+    else {
+        textColorTheme = "text-dark";
+    }
+
     return (
         <>
-            <h6 className="text-center mt-5 font-monospace">
+            <h6 className={`text-center mt-5 font-monospace ${textColorTheme}`}>
                 Empty card,
                 <br /> <br />
-                Place add your new location for using our application. ;)
+                Place add your new location for using our application.
             </h6>
         </>
     )
